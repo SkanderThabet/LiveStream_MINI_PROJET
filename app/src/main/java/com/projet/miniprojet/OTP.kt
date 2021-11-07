481df32f98b0a44b878e7786fdde654da7f67cfd
Skander THABET <Skander.THABET@polytechnicien.tn>
2021-11-07 16:47:59 +0100

    First init for the project and init resources assets

---
+++ app/src/main/java/com/projet/miniprojet/OTP.kt
@@ -0,0 +1,11 @@
+package com.projet.miniprojet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OTP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp2)
+    }
+}
