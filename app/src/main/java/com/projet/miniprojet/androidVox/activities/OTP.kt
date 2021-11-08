46d956f8c33ee7059d96d2e3d42cdf148815abb5
Skander THABET <Skander.THABET@polytechnicien.tn>
2021-11-08 13:54:25 +0100

    Working on OTP

--- app/src/main/java/com/projet/miniprojet/androidVox/activities/OTP.kt
+++ app/src/main/java/com/projet/miniprojet/androidVox/activities/OTP.kt
@@ -1,7 +1,8 @@
-package com.projet.miniprojet
+package com.projet.miniprojet.androidVox.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.projet.miniprojet.R
 
 class OTP : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
