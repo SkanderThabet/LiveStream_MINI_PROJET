3c26aecb0ab69b8955768842a05a315d29bcfd4e
Skander THABET <Skander.THABET@polytechnicien.tn>
2021-11-10 10:10:34 +0100

    Added new activties

---
+++ app/src/main/java/com/projet/miniprojet/androidVox/activities/profileSettings/ProfileSettings.kt
@@ -0,0 +1,12 @@
+package com.projet.miniprojet.androidVox.activities.profileSettings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.projet.miniprojet.R
+
+class ProfileSettings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_settings)
+    }
+}
---
+++ app/src/main/java/com/projet/miniprojet/androidVox/models/User.kt
@@ -0,0 +1,4 @@
+package com.projet.miniprojet.androidVox.models
+
+class User {
+}
