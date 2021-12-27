1b73745e4963e8cc41447da13bda0c92f2f85cc0
Skander Thabet <skanderthabet@Skanders-MacBook-Pro.local>
2021-12-28 00:57:38 +0100

    changed login backend

--- app/src/main/java/com/projet/miniprojet/androidVox/activities/SignInUp/Sign_In.kt
+++ app/src/main/java/com/projet/miniprojet/androidVox/activities/SignInUp/Sign_In.kt
@@ -2,15 +2,22 @@ package com.projet.miniprojet.androidVox.activities.SignInUp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.android.volley.*
+import com.android.volley.toolbox.HttpHeaderParser
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.projet.miniprojet.androidVox.R
-import com.projet.miniprojet.androidVox.TAG
 import com.projet.miniprojet.androidVox.activities.Homepage.HomePage
-import com.projet.miniprojet.androidVox.voxApp
-import io.realm.mongodb.Credentials
+import kotlinx.android.synthetic.main.activity_profile_compelation.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.UnsupportedEncodingException
+import java.nio.charset.Charset
+
 
 class Sign_In : AppCompatActivity() {
 
@@ -19,7 +26,85 @@ class Sign_In : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        SignInButton.setOnClickListener { login(false) }
+        SignInButton.setOnClickListener {
+            if (validate()) {
+                loginUser()
+            }
+        }
+    }
+
+    private fun loginUser() {
+        progressBar_login.isVisible=true
+
+        val params: HashMap<String, String> = HashMap()
+        params["email"] = textinputEmail.text.toString()
+        params["password"] = textinputpass.text.toString()
+
+        val apiKey = "https://voxappli.herokuapp.com/api/vox/auth/login"
+
+        val jsonObjectRequest: JsonObjectRequest = object : JsonObjectRequest(
+            Request.Method.POST,
+            apiKey, JSONObject(params as Map<*, *>?),
+            Response.Listener<JSONObject?> { response ->
+                try {
+                    if (response.getBoolean("success")) {
+                        val token = response.getString("token")
+                        //                            sharedPreferenceClass.setValue_string("token", token)
+                        Toast.makeText(this@Sign_In, token, Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this@Sign_In, HomePage::class.java))
+                    }
+                    progressBar_login.isVisible=false
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                    progressBar_login.isVisible=false
+                }
+            }, Response.ErrorListener { error ->
+                val response = error.networkResponse
+                if (error is ServerError && response != null) {
+                    try {
+                        val res = String(
+                            response.data,
+                            Charset.forName(HttpHeaderParser.parseCharset(response.headers, "utf-8"))
+                        )
+                        val obj = JSONObject(res)
+                        Toast.makeText(
+                            this@Sign_In,
+                            obj.getString("msg"),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        progressBar_login.isVisible=false
+                    } catch (je: JSONException) {
+                        je.printStackTrace()
+                        progressBar_login.isVisible=false
+                    } catch (je: UnsupportedEncodingException) {
+                        je.printStackTrace()
+                        progressBar_login.isVisible=false
+                    }
+                }
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers: HashMap<String, String> = HashMap()
+                headers["Content-Type"] = "application/json"
+                return params
+            }
+        }
+
+        // set retry policy
+
+        // set retry policy
+        val socketTime = 3000
+        val policy: RetryPolicy = DefaultRetryPolicy(
+            socketTime,
+            DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        )
+        jsonObjectRequest.retryPolicy = policy
+
+        // request add
+
+        // request add
+        val requestQueue = Volley.newRequestQueue(this)
+        requestQueue.add(jsonObjectRequest)
     }
 
 
@@ -28,63 +113,33 @@ class Sign_In : AppCompatActivity() {
         moveTaskToBack(true)
     }
 
-    private fun onLoginSuccess() {
-        // successful login ends this activity, bringing the user back to the project activity
-        startActivity(Intent(this,HomePage::class.java))
-        finish()
-    }
 
-    private fun onLoginFailed(errorMsg: String) {
-        Log.e(TAG(), errorMsg)
-        Toast.makeText(baseContext, errorMsg, Toast.LENGTH_LONG).show()
-    }
+//    private fun validateCredentials(): Boolean = when {
+//        // zero-length usernames and passwords are not valid (or secure), so prevent users from creating accounts with those client-side.
+//        textinputEmail.text.toString().isEmpty() -> false
+//        textinputpass.text.toString().isEmpty() -> false
+//        else -> true
+//    }
 
-    private fun validateCredentials(): Boolean = when {
-        // zero-length usernames and passwords are not valid (or secure), so prevent users from creating accounts with those client-side.
-        textinputEmail.text.toString().isEmpty() -> false
-        textinputpass.text.toString().isEmpty() -> false
-        else -> true
-    }
+    private fun validate(): Boolean {
+        textinputpass.error = null
+        textinputEmail.error = null
 
-    // handle user authentication (login) and account creation
-    private fun login(createUser: Boolean) {
-        if (!validateCredentials()) {
-            onLoginFailed("Invalid username or password")
-            return
+        if (password_et.editText!!.text!!.isEmpty()) {
+            textinputpass.error = "Must not be empty"
+            password_et.error = "Must not be empty"
+            return false
         }
 
-        // while this operation completes, disable the buttons to login or create a new account
-        SignInButton.isEnabled = false
+        if (username_et.editText!!.text!!.isEmpty()) {
+            textinputEmail.error = "Must not be empty"
+            username_et.error = "Must not be empty"
 
-        val username = this.textinputEmail.text.toString()
-        val password = this.textinputpass.text.toString()
-
-
-        if (createUser) {
-            // register a user using the Realm App we created in the TaskTracker class
-            voxApp.emailPassword.registerUserAsync(username, password) {
-                // re-enable the buttons after user registration returns a result
-                SignInButton.isEnabled = true
-                if (!it.isSuccess) {
-                    onLoginFailed("Could not register user.")
-                    Log.e(TAG(), "Error: ${it.error}")
-                } else {
-                    Log.i(TAG(), "Successfully registered user.")
-                    // when the account has been created successfully, log in to the account
-                    login(false)
-                }
-            }
-        } else {
-            val creds = Credentials.emailPassword(username, password)
-            voxApp.loginAsync(creds) {
-                // re-enable the buttons after user login returns a result
-                SignInButton.isEnabled = true
-                if (!it.isSuccess) {
-                    onLoginFailed(it.error.message ?: "An error occurred.")
-                } else {
-                    onLoginSuccess()
-                }
-            }
+            return false
         }
+
+        return true
     }
+
+
 }
